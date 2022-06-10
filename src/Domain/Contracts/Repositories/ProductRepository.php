bac818b0fbd9ac519af0f0f0a1e621a093d715f8
Marlon P. Dias <marlondias@users.noreply.github.com>
2022-06-09 21:29:41 -0300

    domain layer

---
+++ src/Domain/Contracts/Repositories/ProductRepository.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace TheSource\Domain\Contracts\Repositories;
+
+use TheSource\Domain\Entities\Product;
+
+interface ProductRepository
+{
+    /**
+     * @param integer $amount
+     * @return Product[]
+     */
+    public function getRandomProducts(int $amount): array;
+
+    public function insertProduct(Product $product): bool;
+}
