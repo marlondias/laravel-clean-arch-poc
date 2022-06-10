eaa1aab199b4a52be9722b02b04924d3ab08d697
Marlon P. Dias <marlondias@users.noreply.github.com>
2022-06-09 21:30:24 -0300

    boundary interfaces

---
+++ src/Application/UseCases/Contracts/InputBoundaryInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace TheSource\Application\UseCases\Contracts;
+
+interface InputBoundaryInterface
+{
+}
+
---
+++ src/Application/UseCases/Contracts/OutputBoundaryInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace TheSource\Application\UseCases\Contracts;
+
+interface OutputBoundaryInterface
+{
+    public function toArray(): array;
+}
+
