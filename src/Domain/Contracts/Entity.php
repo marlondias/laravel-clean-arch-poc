a6b493492d1bd068e1fdd6c3f4c02f32a38fdc37
Marlon P. Dias <marlondias@users.noreply.github.com>
2022-06-12 07:10:08 -0300

    base enbtity

---
+++ src/Domain/Contracts/Entity.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace TheSource\Domain\Contracts;
+
+use stdClass;
+
+abstract class Entity
+{
+    protected int $id;
+
+    public function getId(): ?int
+    {
+        return isset($this->id) ? $this->id : null;
+    }
+
+    public function setId(int $value): self
+    {
+        $this->id = $value;
+        return $this;
+    }
+
+    abstract public function toArray(): array;
+
+    abstract public function toObject(): stdClass;
+
+}
