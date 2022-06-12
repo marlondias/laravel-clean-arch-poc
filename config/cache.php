76f0142557fb6ba8b934b846843f2e4eda242bfd
Marlon P. Dias <marlondias@users.noreply.github.com>
2022-06-11 23:01:08 -0300

    cache configs

--- config/cache.php
+++ config/cache.php
@@ -33,9 +33,9 @@ return [
 
     'stores' => [
 
-        'apc' => [
-            'driver' => 'apc',
-        ],
+        // 'apc' => [
+        //     'driver' => 'apc',
+        // ],
 
         'array' => [
             'driver' => 'array',
@@ -48,30 +48,36 @@ return [
             'connection' => null,
             'lock_connection' => null,
         ],
+        'database2' => [
+            'driver' => 'database',
+            'table' => 'cache',
+            'connection' => null,
+            'lock_connection' => null,
+        ],
 
         'file' => [
             'driver' => 'file',
             'path' => storage_path('framework/cache/data'),
         ],
 
-        'memcached' => [
-            'driver' => 'memcached',
-            'persistent_id' => env('MEMCACHED_PERSISTENT_ID'),
-            'sasl' => [
-                env('MEMCACHED_USERNAME'),
-                env('MEMCACHED_PASSWORD'),
-            ],
-            'options' => [
-                // Memcached::OPT_CONNECT_TIMEOUT => 2000,
-            ],
-            'servers' => [
-                [
-                    'host' => env('MEMCACHED_HOST', '127.0.0.1'),
-                    'port' => env('MEMCACHED_PORT', 11211),
-                    'weight' => 100,
-                ],
-            ],
-        ],
+        // 'memcached' => [
+        //     'driver' => 'memcached',
+        //     'persistent_id' => env('MEMCACHED_PERSISTENT_ID'),
+        //     'sasl' => [
+        //         env('MEMCACHED_USERNAME'),
+        //         env('MEMCACHED_PASSWORD'),
+        //     ],
+        //     'options' => [
+        //         // Memcached::OPT_CONNECT_TIMEOUT => 2000,
+        //     ],
+        //     'servers' => [
+        //         [
+        //             'host' => env('MEMCACHED_HOST', '127.0.0.1'),
+        //             'port' => env('MEMCACHED_PORT', 11211),
+        //             'weight' => 100,
+        //         ],
+        //     ],
+        // ],
 
         'redis' => [
             'driver' => 'redis',
@@ -79,18 +85,18 @@ return [
             'lock_connection' => 'default',
         ],
 
-        'dynamodb' => [
-            'driver' => 'dynamodb',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
-            'table' => env('DYNAMODB_CACHE_TABLE', 'cache'),
-            'endpoint' => env('DYNAMODB_ENDPOINT'),
-        ],
+        // 'dynamodb' => [
+        //     'driver' => 'dynamodb',
+        //     'key' => env('AWS_ACCESS_KEY_ID'),
+        //     'secret' => env('AWS_SECRET_ACCESS_KEY'),
+        //     'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
+        //     'table' => env('DYNAMODB_CACHE_TABLE', 'cache'),
+        //     'endpoint' => env('DYNAMODB_ENDPOINT'),
+        // ],
 
-        'octane' => [
-            'driver' => 'octane',
-        ],
+        // 'octane' => [
+        //     'driver' => 'octane',
+        // ],
 
     ],
 
