25521e94100b5147e4f598ad26a4f3f8b92d34ff
Marlon P. Dias <marlondias@users.noreply.github.com>
2022-06-24 22:48:07 -0300

    created entity for Product Category

---
+++ src/Domain/Entities/ProductCategory.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace TheSource\Domain\Entities;
+
+use DateTime;
+use TheSource\Domain\Contracts\Entity;
+
+class ProductCategory extends Entity
+{
+    protected string $name; //unique
+    protected ?DateTime $createdAt = null;
+    protected ?DateTime $updatedAt = null;
+
+
+    public function toArray(): array
+    {
+        return [
+            'id' => $this->id ?? null,
+            'name' => $this->name ?? null,
+            'createdAt' => $this->createdAt?->format('Y-m-d H:i:s'),
+            'updatedAt' => $this->updatedAt?->format('Y-m-d H:i:s'),
+        ];
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getCreatedAt(): ?DateTime
+    {
+        return $this->createdAt;
+    }
+
+    public function getUpdatedAt(): ?DateTime
+    {
+        return $this->updatedAt;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function setCreatedAt(string $dateTimeYMD): self
+    {
+        $this->createdAt = DateTime::createFromFormat('Y-m-d H:i:s', $dateTimeYMD);
+        return $this;
+    }
+
+    public function setUpdatedAt(string $dateTimeYMD): self
+    {
+        $this->updatedAt = DateTime::createFromFormat('Y-m-d H:i:s', $dateTimeYMD);
+        return $this;
+    }
+}
