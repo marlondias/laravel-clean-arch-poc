b7610fbc3435e1013b7f1874966783a3db84c34a
Marlon P. Dias <marlondias@users.noreply.github.com>
2022-07-08 23:09:30 -0300

    added email validation to use case

--- src/Application/UseCases/User/CreateUser/CreateUserUseCase.php
+++ src/Application/UseCases/User/CreateUser/CreateUserUseCase.php
@@ -2,21 +2,26 @@
 
 namespace TheSource\Application\UseCases\User\CreateUser;
 
+use DomainException;
 use TheSource\Application\Contracts\UseCaseInteractor;
 use TheSource\Domain\Contracts\Repositories\User\UserCommandsRepository;
+use TheSource\Domain\Contracts\Repositories\User\UserQueriesRepository;
 use TheSource\Domain\Contracts\Services\StringHashingService;
 use TheSource\Domain\Entities\User;
 
 final class CreateUserUseCase implements UseCaseInteractor
 {
     private UserCommandsRepository $userCommandsRepository;
+    private UserQueriesRepository $userQueriesRepository;
     private StringHashingService $stringHashingService;
 
     public function __construct(
         UserCommandsRepository $userCommandsRepository,
+        UserQueriesRepository $userQueriesRepository,
         StringHashingService $stringHashingService
     ) {
         $this->userCommandsRepository = $userCommandsRepository;
+        $this->userQueriesRepository = $userQueriesRepository;
         $this->stringHashingService = $stringHashingService;
     }
 
@@ -27,6 +32,15 @@ final class CreateUserUseCase implements UseCaseInteractor
             ->setPersonName($input->getFirstName(), $input->getLastName())
             ->setEmailAddress($input->getEmailAddress())
             ->setHashedPasswordFromPlainText($this->stringHashingService, $input->getPassword());
+
+        $isEmailInUse = $user->isEmailAddressAlreadyInUse(
+            $this->userQueriesRepository,
+            $user->getEmailAddress()
+        );
+
+        if ($isEmailInUse) {
+            throw new DomainException('Já existe um usuário com o e-mail informado.');
+        }
 
         $this->userCommandsRepository->insert($user);
 
